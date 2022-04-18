89e24e31e2e098ef91ca3ab370fef984f3295da2
faderz <58748338+faderzz@users.noreply.github.com>
2022-04-18 02:05:51 +0100

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -6,55 +6,37 @@ import dribbble from '../assets/dribbble.png'
 import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
 import twitter from '../assets/twitter.png'
+import behance from '../assets/behance-3-logo-png-transparent.png'
 
 const items = [{
-    "title": "Website",
-    "subtitle": "Look at my work!",
-    "image": website,
-    "link": "https://sagniksahoo.codes" //your personal website or portfolio  link
+    "title": "YouTube",
+    "subtitle": "Look at my YouTube channel!",
+    "image": youtube,
+    "link": "https://www.youtube.com/channel/UC3W-Of0TqVbPwXRIYGvMC0w?sub_confirmation=1" //your personal website or portfolio  link
 },
 {
     "title": "GitHub",
-    "subtitle": "@heysagnik | 🏠 of my open-source projects",
+    "subtitle": "@faderzz - Find my projects here.",
     "image": github,
     "link": "https://github.com/heysagnik" //Github Profile link
 },
 {
     "title": "Instagram",
-    "subtitle": "@heysagnik | Hub of my awesome 😎 pics",
+    "subtitle": "@_faderz - I share my exclusive work here. 🙈",
     "image": instagram,
     "link": "https://instagram.com/heysagnik" //instagram profile link 
 },
 {
-    "title": "Twitter",
-    "subtitle": "@heysagnik | Don't forget to follow me 😉",
+    "title": "Behance",
+    "subtitle": "My portfolio containing my past work!",
     "image": twitter,
     "link": "https://twitter.com/heysagnik"// twitter profile link 
 },
 {
-    "title": "Apps",
-    "subtitle": "Hub of my awesome 🔥 Apps",
-    "image": appstore,
+    "title": "Discord",
+    "subtitle": "Add me on Discord! - faderz#1501",
+    "image": discord,
     "link": "#"//
-},
-{
-    "title": "YouTube",
-    "subtitle": "CODER SAGNIK | Official channel of mine",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
-},
-
-{
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
-},
-{
-    "title": "Telegram",
-    "subtitle": "@heysagnik | Chat with me instantly, Don't hesitate! ",
-    "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
 }]
 
 export default items
